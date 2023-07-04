4717eaba173249933501b0e349ea157e334469db
TrieuPN <bettertogetheronit@gmail.com>
2023-07-05 00:06:32 +0700

    Update Testing

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include "graph.h"
 
 
-int main() {
-    // Demo for undirected graph -> we don't need to make reverse
+/**
+ *      0 ------ 1
+ *      |       /
+ *      |     /
+ *      |    /
+ *      |  /
+ *       2 
+*/
+void RunUndirectedTest() {
+    // Generating undireted graph with 3 vertices
     auto graph = undirected_graph_api::getInstance(3);
-    graph->init();
-    graph->insertVertice(0, 1);
-    graph->insertVertice(0, 2);
-    graph->insertVertice(1, 2);
-    graph->insertVertice(3, 2);
+    graph->init();              // Initilizing list
+    graph->insertVertice(0, 1); // Worked
+    graph->insertVertice(0, 2); // Worked
+    graph->insertVertice(1, 2); // Worked
+    graph->insertVertice(3, 2); // Could not be inserted due to out range
     graph->printGraphic();
+}
 
+int main() {
+    RunUndirectedTest();
     return 0;
 }
